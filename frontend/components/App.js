41997637c9e337cbfea3f40d7b32f458979d163a
JacobBarnett <93158517+JacobBarnett@users.noreply.github.com>
2025-04-19 18:48:21 -0700

    finished project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,52 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React from "react";
+import axios from "axios";
+import Character from "./Character";
+import { useEffect, useState } from "react";
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = "http://localhost:9009/api/planets";
+const urlPeople = "http://localhost:9009/api/people";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:9009/api/people"),
+      axios.get("http://localhost:9009/api/planets"),
+    ])
+      .then(([res1, res2]) => {
+        setPeople(res1.data);
+        setPlanets(res2.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      <p>
+        See the README of the project for instructions on completing this
+        challenge
+      </p>
+      {people.map((person) => {
+        const homeworld = planets.find(
+          (planet) => planet.id === person.homeworld
+        );
+        return (
+          <Character
+            key={person.id}
+            name={person.name}
+            homeworld={homeworld?.name || "Unknown"}
+          />
+        );
+      })}{" "}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = App
+if (typeof module !== "undefined" && module.exports) module.exports = App;
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
+function Character({ name, homeworld }) {
+  const [showHomeworld, setShowHomeworld] = useState(false);
+
+  const toggleHomeworld = () => {
+    setShowHomeworld((prev) => !prev);
+  };
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card" onClick={toggleHomeworld}>
+      <h3 className="character-name">{name}</h3>
+      {showHomeworld && <p className="character-planet">Planet: {homeworld}</p>}
     </div>
-  )
+  );
 }
 
-export default Character
+export default Character;
